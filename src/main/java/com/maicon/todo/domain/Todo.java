f9f49c60f6802d96dc35671a4ae5c6e9fd4a2222
Maicon Cardoso <maiconscardoso@hotmail.com>
2021-10-26 07:05:33 -0300

    Initial Commit

---
+++ src/main/java/com/maicon/todo/domain/Todo.java
@@ -0,0 +1,29 @@
+package com.maicon.todo.domain;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+public class Todo implements Serializable {
+    private static final long serialVersionUID = 1L;
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String title;
+    private String description;
+    private LocalDateTime finishedDate;
+    private Boolean finished = false;
+}
